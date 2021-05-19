66cf12c5496b428ac192b85d88de1e24db667c7c
James Morgan <MorganJay@users.noreply.github.com>
2021-05-19 18:42:17 +0100

    adding repository pattern

--- CustomerMicroservice/Controllers/CustomersController.cs
+++ CustomerMicroservice/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
-﻿using Dapper;
+﻿using Customer.Microservice.Repositories;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
@@ -15,84 +16,59 @@ namespace Customer.Microservice.Controllers
     [ApiController]
     public class CustomersController : ControllerBase
     {
-        private readonly IConfiguration _configuration;
-
-        private string ConnectionString { get; }
-        //private readonly string connectionString =  "Data Source=DESKTOPMORGANJA;Initial Catalog=CustomerDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+        private readonly ProductRepository productRepository;
 
         public CustomersController(IConfiguration configuration)
         {
-            _configuration = configuration;
-            ConnectionString = _configuration.GetConnectionString("DefaultConnection");
+            productRepository = new ProductRepository(configuration);
         }
 
         // GET: api/<CustomersController>
         [HttpGet]
-        public async Task<IEnumerable<Models.Customer>> GetAll()
+        public async Task<IActionResult> GetCustomers()
         {
-            IEnumerable<Models.Customer> customers;
-
-            using (SqlConnection connection = new(ConnectionString))
-            {
-                await connection.OpenAsync();
-                var sqlQuery = "SELECT * FROM Customer";
-                customers = await connection.QueryAsync<Models.Customer>(sqlQuery);
-            }
-
-            return customers;
+            return Ok(await productRepository.GetAll());
         }
 
         // GET api/<CustomersController>/5
         [HttpGet("{id}")]
-        public async Task<Models.Customer> Get(int id)
+        public async Task<IActionResult> GetCustomer(int id)
         {
-            Models.Customer customer = new();
-            using (SqlConnection connection = new(ConnectionString))
-            {
-                await connection.OpenAsync();
-                var sqlQuery = "SELECT * FROM Customer WHERE Id = @Id";
-                customer = await connection.QuerySingleAsync<Models.Customer>(sqlQuery, new { Id = id });
-            }
-
-            return customer;
+            return Ok(await productRepository.GetById(id));
         }
 
         // POST api/<CustomersController>
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] Models.Customer customer)
+        public async Task<IActionResult> CreateCustomer([FromBody] Models.Customer customer)
         {
-            using (SqlConnection connection = new(ConnectionString))
+            if (ModelState.IsValid)
             {
-                await connection.OpenAsync();
-                var sqlQuery = "INSERT INTO Customer (Name, Address, Telephone, Email) Values(@Name, @Address, @Telephone, @Email)";
-                await connection.ExecuteAsync(sqlQuery, customer);
+                var model = await productRepository.Add(customer);
+                return Ok(model);
             }
-            return Ok(customer);
+            return BadRequest();
         }
 
         // PUT api/<CustomersController>/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(int id, [FromBody] Models.Customer customer)
+        public IActionResult UpdateCustomer(int id, [FromBody] Models.Customer customer)
         {
-            using (SqlConnection connection = new(ConnectionString))
+            if (ModelState.IsValid)
             {
-                await connection.OpenAsync();
-                var sqlQuery = "UPDATE Customer SET Name = @Name, Address = @Address, Email = @Email WHERE Id = @Id";
-                await connection.ExecuteAsync(sqlQuery, customer);
+                productRepository.Update(id, customer);
+                return Ok();
             }
-            return Ok();
+            return BadRequest();
         }
 
         // DELETE api/<CustomersController>/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
+        public async Task<IActionResult> DeleteCustomer(int id)
         {
-            using (SqlConnection connection = new(ConnectionString))
-            {
-                await connection.OpenAsync();
-                var sqlQuery = "DELETE FROM Customer WHERE Id = @Id";
-                await connection.ExecuteAsync(sqlQuery, new { Id = id });
-            }
+            var customer = await productRepository.GetById(id);
+            if (customer is null) return NotFound();
+
+            productRepository.Delete(id);
             return Ok();
         }
     }
---
+++ CustomerMicroservice/Repositories/ProductRepository.cs
@@ -0,0 +1,72 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customer.Microservice.Repositories
+{
+    public class ProductRepository
+    {
+        private string ConnectionString { get; }
+
+        public ProductRepository(IConfiguration configuration)
+        {
+            ConnectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<IEnumerable<Models.Customer>> GetAll()
+        {
+            using (SqlConnection connection = new(ConnectionString))
+            {
+                var sqlQuery = "SELECT * FROM Customer";
+                await connection.OpenAsync();
+                return await connection.QueryAsync<Models.Customer>(sqlQuery);
+            }
+        }
+
+        public async Task<Models.Customer> GetById(int id)
+        {
+            using (SqlConnection connection = new(ConnectionString))
+            {
+                var sqlQuery = "SELECT * FROM Customer WHERE Id = @Id";
+                await connection.OpenAsync();
+                return await connection.QuerySingleAsync<Models.Customer>(sqlQuery, new { Id = id });
+            }
+        }
+
+        public async Task<Models.Customer> Add(Models.Customer customer)
+        {
+            using (SqlConnection connection = new(ConnectionString))
+            {
+                var sqlQuery = "INSERT INTO Customer (Name, Address, Telephone, Email) Values(@Name, @Address, @Telephone, @Email)";
+                await connection.OpenAsync();
+                await connection.ExecuteAsync(sqlQuery, customer);
+            }
+            return customer;
+        }
+
+        public void Update(int id, Models.Customer customer)
+        {
+            using (SqlConnection connection = new(ConnectionString))
+            {
+                var sqlQuery = "UPDATE Customer SET Name = @Name, Address = @Address, Email = @Email WHERE Id = @Id";
+                connection.OpenAsync();
+                connection.ExecuteAsync(sqlQuery, customer);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection connection = new(ConnectionString))
+            {
+                var sqlQuery = "DELETE FROM Customer WHERE Id = @Id";
+                connection.OpenAsync();
+                connection.ExecuteAsync(sqlQuery, new { Id = id });
+            }
+        }
+    }
+}
